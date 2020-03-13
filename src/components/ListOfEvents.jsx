7a45d1129ff2b7f9de432a5d938ca6a75591adb0
Piotr Florczak <piotrflorczakk@gmail.com>
2020-03-13 19:24:37 +0100

    Dodanie blokow do agendy

--- src/components/ListOfEvents.jsx
+++ src/components/ListOfEvents.jsx
@@ -31,6 +31,87 @@ function ListOfEvents() {
         </div>
         <Link to="/">Dowiedz się więcej</Link>
       </div>
+      <div id="1" className="event shadow">
+        <div className="coordinate">
+          <span className="small-span">
+            Sala
+            <br />
+            L103
+          </span>
+          <span className="small-span">08:00-09:00</span>
+        </div>
+        <p className="bold">Potwierdzenie swoją obecność na konferencji</p>
+        <div className="credentials light-green">
+          <div className="image-wrapper green">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/organisers/sebastian-kowalski.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div>
+            <span>Reset</span>
+            <span className="small-span">Organizatorzy</span>
+          </div>
+        </div>
+        <Link to="/">Dowiedz się więcej</Link>
+      </div>
+      <div id="1" className="event shadow">
+        <div className="coordinate">
+          <span className="small-span">
+            Sala
+            <br />
+            L103
+          </span>
+          <span className="small-span">08:00-09:00</span>
+        </div>
+        <p className="bold">Potwierdzenie swoją obecność na konferencji</p>
+        <div className="credentials light-green">
+          <div className="image-wrapper green">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/organisers/sebastian-kowalski.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div>
+            <span>Reset</span>
+            <span className="small-span">Organizatorzy</span>
+          </div>
+        </div>
+        <Link to="/">Dowiedz się więcej</Link>
+      </div>
+      <div id="1" className="event shadow">
+        <div className="coordinate">
+          <span className="small-span">
+            Sala
+            <br />
+            L103
+          </span>
+          <span className="small-span">08:00-09:00</span>
+        </div>
+        <p className="bold">Potwierdzenie swoją obecność na konferencji</p>
+        <div className="credentials light-green">
+          <div className="image-wrapper green">
+            <img
+              src={
+                process.env.PUBLIC_URL +
+                "/images/organisers/sebastian-kowalski.jpg"
+              }
+              alt=""
+            />
+          </div>
+          <div>
+            <span>Reset</span>
+            <span className="small-span">Organizatorzy</span>
+          </div>
+        </div>
+        <Link to="/">Dowiedz się więcej</Link>
+      </div>
     </div>
   );
 }
