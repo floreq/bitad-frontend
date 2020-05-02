ff889f0fccaef5cd5287c697f335a0bc0842c54c
Piotr Florczak <piotrflorczakk@gmail.com>
2020-05-02 21:38:17 +0200

    Funkcjonalnosc menu hamburger

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,53 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
 import ResetLogo from "./../assets/images/reset-logo.svg";
 
 function Navbar() {
+  const [ifOpen, setOpen] = useState(false);
+  const noScroll = (e) => {
+    const html = document.querySelector("html");
+    if (e) {
+      html.className = "no-scroll";
+    } else {
+      html.className = "";
+    }
+  };
+  const onClickLink = () => {
+    if (ifOpen) {
+      setOpen(false);
+      noScroll(!ifOpen);
+    }
+  };
   return (
-    <nav>
+    <nav className={ifOpen ? "open" : ""}>
       <div className="container">
-        <NavLink to={process.env.PUBLIC_URL + "/"} className="nav-link">
+        <NavLink
+          to={process.env.PUBLIC_URL + "/"}
+          className="nav-link"
+          onClick={onClickLink}
+        >
           <img src={ResetLogo} alt="Reset logo" />
         </NavLink>
         <div>
-          <ul className="nav-links">
-            <li>
-              <NavLink
-                exact
-                to={process.env.PUBLIC_URL + "/"}
-                className="nav-link"
-              >
-                O konferencji
-              </NavLink>
-            </li>
-            <li>
-              <HashLink
-                to={process.env.PUBLIC_URL + "/#sponsors"}
-                className="nav-link"
-              >
-                Sponsorzy
-              </HashLink>
-            </li>
-            <li>
-              <NavLink
-                to={process.env.PUBLIC_URL + "/agenda"}
-                className="nav-link"
-              >
-                Agenda
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to={process.env.PUBLIC_URL + "/rejestracja"}
-                className="nav-link button"
-              >
-                Rejestracja
-              </NavLink>
-            </li>
-          </ul>
-          <div className="hambuerger">
-            <input type="checkbox" />
+          <div className="nav-links">
+            <NavLink
+              exact
+              to={process.env.PUBLIC_URL + "/"}
+              className="nav-link"
+              onClick={onClickLink}
+            >
+              O konferencji
+            </NavLink>
+
+            <HashLink
+              to={process.env.PUBLIC_URL + "/#sponsors"}
+              className="nav-link"
+              onClick={onClickLink}
+            >
+              Sponsorzy
+            </HashLink>
+
+            <NavLink
+              to={process.env.PUBLIC_URL + "/agenda"}
+              className="nav-link"
+              onClick={onClickLink}
+            >
+              Agenda
+            </NavLink>
+
+            <NavLink
+              to={process.env.PUBLIC_URL + "/rejestracja"}
+              className="nav-link button"
+              onClick={onClickLink}
+            >
+              Rejestracja
+            </NavLink>
+          </div>
+          <div
+            className="hambuerger"
+            onClick={() => {
+              setOpen(!ifOpen);
+              noScroll(!ifOpen);
+            }}
+          >
             <span></span>
             <span></span>
             <span></span>
